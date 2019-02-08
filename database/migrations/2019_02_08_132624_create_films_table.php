71200749ffa1dc082fa958f65501124cc8bb971f
Kareema <kareema.zohra@gmail.com>
2019-02-08 20:02:28 +0600

    Backend 1.1 completed
    required model , view and controls created for film page.
    Database for project named films created.
    In the database a table named films created. fields are created with their respective data types of best practice.
    able has been uploaded to database using migration.

---
+++ database/migrations/2019_02_08_132624_create_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('fname');
+            $table->text('description');
+            $table->date('release_date');
+            $table->integer('rating');
+            $table->integer('ticket_price');
+            $table->string('country');
+            $table->string('genere');
+            $table->binary('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
